dce07624ff91af43f0c2e88b664e4289b4d22fe8
Sufike <l.christ54260@gmail.com>
2019-04-22 07:48:45 +0200

    fix the message

--- src/main/java/fr/pumpmykins/pumpmyprefix/utils/MessageEventHandler.java
+++ src/main/java/fr/pumpmykins/pumpmyprefix/utils/MessageEventHandler.java
@@ -1,8 +1,7 @@
 package fr.pumpmykins.pumpmyprefix.utils;
 
 import fr.pumpmykins.pumpmyprefix.ChatPlayer;
-import fr.pumpmykins.pumpmyprefix.Formator;
-import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ChatEvent;
@@ -29,21 +28,59 @@ public class MessageEventHandler implements Listener {
 		
 		String prefix = new String();
 		String nickname = new String();
-			
+		
 		if(!this.chatPlayer.getPrefix().isEmpty()) {
 			prefix = this.chatPlayer.getPrefix().get(player.getUniqueId());
+			if(prefix == null)
+				prefix = "";
 		}
 		if(!this.chatPlayer.getNickname().isEmpty()) {
 			nickname = this.chatPlayer.getNickname().get(player.getUniqueId());
+			if(nickname == null)
+				nickname = player.getDisplayName();	
 		}
 			
-		if(nickname == null)
-			nickname = player.getDisplayName();	
 			
-		TextComponent message = Formator.format(player, nickname ,prefix , event.getMessage());
+		String message = event.getMessage();
+		
+		ChatColor.translateAlternateColorCodes('&', prefix);
+		
+		if(player.hasPermission("rank.tier2") || player.hasPermission("rank.tier3")) {
+			ChatColor.translateAlternateColorCodes('&', message);
+		}
+		
+		TextComponent messages = new TextComponent();
+		
+		TextComponent name = new TextComponent(nickname);
+		
+		TextComponent bet = new TextComponent(" > ");
+		bet.setColor(ChatColor.GOLD);
+		
+		if(!prefix.isEmpty()) {
 			
+			TextComponent tcStartPrefix = new TextComponent("[");
+			TextComponent tcEndPrefix = new TextComponent("] ");
+			TextComponent tcPrefix = new TextComponent(prefix);
+			
+			tcStartPrefix.addExtra(tcPrefix);
+			tcStartPrefix.addExtra(tcEndPrefix);
+			tcStartPrefix.addExtra(" ");
+			tcStartPrefix.addExtra(name);
+			tcStartPrefix.addExtra(bet);
+			tcStartPrefix.addExtra(message);
+			
+			messages = tcStartPrefix;
+		} else {
+			
+			name.addExtra(bet);
+			name.addExtra(message);
+			
+			messages = name;
+		}
+		
+		
 		for (ProxiedPlayer receiver : player.getServer().getInfo().getPlayers()) {
-			receiver.sendMessage(message);
+			receiver.sendMessage(messages);
 		}
 		
 	}
