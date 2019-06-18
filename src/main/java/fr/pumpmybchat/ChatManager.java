df961279097431468486beaf8771dceb4d0cd11c
Clem-Fern <clemfern456@gmail.com>
2019-06-18 23:58:58 +0200

    Update imports in ChatManager class

--- src/main/java/fr/pumpmybchat/ChatManager.java
+++ src/main/java/fr/pumpmybchat/ChatManager.java
@@ -1,6 +1,10 @@
 package fr.pumpmybchat;
 
+import java.sql.ResultSet;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
