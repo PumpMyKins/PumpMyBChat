35110a6a9642e4a4f5dde66893e748a09108aa89
Clem-Fern <clemfern456@gmail.com>
2019-06-19 01:25:20 +0200

    Registering nick command in onEnable method in Main class

--- src/main/java/fr/pumpmybchat/Main.java
+++ src/main/java/fr/pumpmybchat/Main.java
@@ -64,24 +64,21 @@ public class Main  extends Plugin implements Listener{
 			return;
 		}
 
-		/*mySQL.openConnection();
-		if(mySQL.isConnected()) {
-
-			String createprefixtable = "CREATE TABLE IF NOT EXISTS PrefixPlayer(`uuid` VARCHAR(191) NOT NULL UNIQUE, `prefix` VARCHAR(191), `active` TINYINT NOT NULL DEFAULT 0, `warn` INT NOT NULL DEFAULT 0, `modification` INT NOT NULL DEFAULT 0)";
-			mySQL.update(createprefixtable);
-
-		}*/
-
-		this.chatPlayer = new ChatPlayer();
 		
-		PluginManager pm = ProxyServer.getInstance().getPluginManager();
+		// COMMANDS
 		
+		pm.registerCommand(this, new NicknameCommand("nick",this.chatManager));
+		
+		/*this.chatPlayer = new ChatPlayer();
+
+
+
 		//START PREFIX COMMAND REGISTERING
 		pm.registerCommand(this, new PrefixCommand("prefix"));
-		
+
 		PrefixCommand.addCommand(Arrays.asList("help", "h"), new PrefixHelpCommand());
 		PrefixCommand.addCommand(Arrays.asList("color"), new PrefixColorCommand());
-		
+
 		//PREFIX COMMAND WHICH REQUIRE A RELOAD
 		PrefixCommand.addCommand(Arrays.asList("set"), new PrefixSetCommand(this.chatPlayer));
 		PrefixCommand.addCommand(Arrays.asList("activate", "a"), new PrefixActivationCommand(this.chatPlayer));
@@ -92,39 +89,10 @@ public class Main  extends Plugin implements Listener{
 		PrefixCommand.addCommand(Arrays.asList("reload", "r"), new PrefixReloadCommand(this.chatPlayer));
 
 		//NICK COMMAND
-		pm.registerCommand(this, new NicknameCommand("nick", this.chatPlayer));
-		
+
+
 		//CHAT FORMATAGE
-		pm.registerListener(this, new MessageEventHandler(this.chatPlayer));
+		pm.registerListener(this, new MessageEventHandler(this.chatPlayer));*/
 	}
 	
-	
-	public static Main getSharedInstance() {
-		return sharedInstance;
-	}
-
-	public static MySql getMySQL() throws SQLException {
-		if(mySQL.isConnected()) {
-			return mySQL;
-		} else {
-			mySQL.refreshConnection();
-			if(mySQL.isConnected()) {
-				return mySQL;
-			}
-		}
-		return null;
-	}
-	
-	public static void setSharedInstance(Main sharedInstance) {
-		Main.sharedInstance = sharedInstance;
-	}
-
-	public static TextComponent getERROR_NO_PREFIX() {
-		
-		TextComponent ERROR_NO_PREFIX = new TextComponent(STRING_ERROR_NO_PREFIX);
-		ERROR_NO_PREFIX.setColor(ChatColor.DARK_RED);
-		ERROR_NO_PREFIX.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "http://store.pumpmykins.eu/"));
-		
-		return ERROR_NO_PREFIX;
-	}
 }
