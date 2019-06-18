47d45d3a069f6c960cd9e05248ce833c05e14307
Clem-Fern <clemfern456@gmail.com>
2019-06-19 01:25:54 +0200

    Adding constructor in Prefix class

--- src/main/java/fr/pumpmybchat/Prefix.java
+++ src/main/java/fr/pumpmybchat/Prefix.java
@@ -10,6 +10,14 @@ public class Prefix {
 	private int warn;
 	private int modification;
 	
+	public Prefix(UUID uuid, String prefix, boolean staff, boolean active, int warn, int modification) {
+		super();
+		this.uuid = uuid;
+		this.prefix = prefix;
+		this.active = active;
+		this.warn = warn;
+		this.modification = modification;
+	}
 	
 	public UUID getUuid() {
 		return uuid;
@@ -40,7 +48,5 @@ public class Prefix {
 	}
 	public void setModification(int modification) {
 		this.modification = modification;
-	}
-	
-	
+	}	
 }
