bc42dad40085d6b34df6bf292b4ec2f52b2110cf
Clem-Fern <clemfern456@gmail.com>
2019-06-19 12:29:15 +0200

    Adding Nickname class

---
+++ src/main/java/fr/pumpmybchat/Nickname.java
@@ -0,0 +1,33 @@
+package fr.pumpmybchat;
+
+public class Nickname {
+
+	private boolean active;
+	private String nickname;
+	
+	public Nickname() {
+		this.active = false;
+		this.nickname = "";
+	}
+
+	public boolean hasOne() {
+		return active;
+	}
+
+	public String getUnSafeNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+		this.active = true;
+	}
+	
+	public void unsetNickname() {
+		this.nickname = "";
+		this.active = false;		
+	}
+	
+	
+	
+}
