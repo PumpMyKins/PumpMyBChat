2e75fd5b0c153ec1567190b1b504e3d70ecc989d
Clem-Fern <clemfern456@gmail.com>
2019-06-19 12:29:32 +0200

    Adding ChatProfile class

---
+++ src/main/java/fr/pumpmybchat/ChatProfile.java
@@ -0,0 +1,36 @@
+package fr.pumpmybchat;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.List;
+
+public class ChatProfile {
+	
+	private Nickname nickname;
+	private Prefix prefix;
+	private List<SimpleEntry<String, String>> prefixHistory;
+	private List<SimpleEntry<String, String>> nickHistory;
+	
+	public ChatProfile(Prefix prefix, List<SimpleEntry<String, String>> prefixHistory, List<SimpleEntry<String, String>> nickHistory) {
+		this.nickname = new Nickname();
+		this.prefix = prefix;
+		this.prefixHistory = prefixHistory;
+		this.nickHistory = nickHistory;
+	}
+
+	public List<SimpleEntry<String, String>> getPrefixHistory() {
+		return prefixHistory;
+	}
+
+	public List<SimpleEntry<String, String>> getNickHistory() {
+		return nickHistory;
+	}
+
+	public Nickname getNickname() {
+		return this.nickname;
+	}
+
+	public Prefix getPrefix() {
+		return this.prefix;
+	}
+
+}
