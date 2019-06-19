0edfc8a5a8dd6cbdb5bb03160f09fd15c826332c
Clem-Fern <clemfern456@gmail.com>
2019-06-19 21:41:22 +0200

    Fixing imports in ChatManager class

--- src/main/java/fr/pumpmybchat/ChatManager.java
+++ src/main/java/fr/pumpmybchat/ChatManager.java
@@ -2,13 +2,17 @@ package fr.pumpmybchat;
 
 import java.sql.ResultSet;
 import java.util.AbstractMap.SimpleEntry;
+import fr.pumpmybchat.logging.CustomLevel;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ChatEvent;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
