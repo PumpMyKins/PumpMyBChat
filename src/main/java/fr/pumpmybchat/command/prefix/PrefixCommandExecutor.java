9a5b3eb109749765f5ce4a5400408be84518ccb1
Clem-Fern <clemfern456@gmail.com>
2019-06-21 18:47:35 +0200

    Update import in PrefixCommandExecutor class

--- src/main/java/fr/pumpmybchat/command/prefix/PrefixCommandExecutor.java
+++ src/main/java/fr/pumpmybchat/command/prefix/PrefixCommandExecutor.java
@@ -12,6 +12,7 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.TabExecutor;
 
