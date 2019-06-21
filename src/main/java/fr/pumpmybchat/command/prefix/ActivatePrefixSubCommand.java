4ca3b79a36904a4940a8340e6f7eb838683e2dab
Clem-Fern <clemfern456@gmail.com>
2019-06-21 23:43:35 +0200

    Update imports in ActivatePrefixSubCommand class

--- src/main/java/fr/pumpmybchat/command/prefix/ActivatePrefixSubCommand.java
+++ src/main/java/fr/pumpmybchat/command/prefix/ActivatePrefixSubCommand.java
@@ -2,9 +2,15 @@ package fr.pumpmybchat.command.prefix;
 
 import java.util.List;
 
+import fr.pumpmybchat.Main;
 import fr.pumpmybchat.chat.ChatManager;
+import fr.pumpmybchat.chat.ChatProfile;
+import fr.pumpmybchat.chat.Prefix;
 import fr.pumpmybchat.command.utils.ISubCommand;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class ActivatePrefixSubCommand implements ISubCommand {
