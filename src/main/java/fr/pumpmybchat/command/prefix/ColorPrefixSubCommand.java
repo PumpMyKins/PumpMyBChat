ae296d2ff2f8192f8b2b04bb9fa610045963527b
Clem-Fern <clemfern456@gmail.com>
2019-06-21 16:21:32 +0200

    Creating ColorPrefixSubCommand class

---
+++ src/main/java/fr/pumpmybchat/command/prefix/ColorPrefixSubCommand.java
@@ -0,0 +1,17 @@
+package fr.pumpmybchat.command.prefix;
+
+import java.util.List;
+
+import fr.pumpmybchat.utils.ISubCommand;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+public class ColorPrefixSubCommand implements ISubCommand {
+
+	@Override
+	public void onSubCommand(Command exec, CommandSender sender, List<String> args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
