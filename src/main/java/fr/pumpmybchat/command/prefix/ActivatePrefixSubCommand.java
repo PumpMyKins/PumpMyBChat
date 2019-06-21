89ecd7760efeca66db2554b50b194fa40a0893b7
Clem-Fern <clemfern456@gmail.com>
2019-06-21 16:21:56 +0200

    Creating ActivatePrefixSubCommand class

---
+++ src/main/java/fr/pumpmybchat/command/prefix/ActivatePrefixSubCommand.java
@@ -0,0 +1,23 @@
+package fr.pumpmybchat.command.prefix;
+
+import java.util.List;
+
+import fr.pumpmybchat.ChatManager;
+import fr.pumpmybchat.utils.ISubCommand;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+public class ActivatePrefixSubCommand implements ISubCommand {
+
+	private ChatManager chatManager;
+
+	public ActivatePrefixSubCommand(ChatManager chatManager) {
+		this.chatManager = chatManager;
+	}
+
+	@Override
+	public void onSubCommand(Command exec, CommandSender sender, List<String> args) {
+
+	}
+
+}
