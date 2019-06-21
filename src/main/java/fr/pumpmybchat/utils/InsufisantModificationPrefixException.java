8cfa78a48750a5ecaa91659b530f85179a5707e1
Clem-Fern <clemfern456@gmail.com>
2019-06-21 18:49:07 +0200

    Addind InsufisantModificationPrefixException class

---
+++ src/main/java/fr/pumpmybchat/utils/InsufisantModificationPrefixException.java
@@ -0,0 +1,11 @@
+package fr.pumpmybchat.utils;
+
+import fr.pumpmybchat.Prefix;
+
+public class InsufisantModificationPrefixException extends Exception {
+
+	public InsufisantModificationPrefixException(Prefix prefix) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
