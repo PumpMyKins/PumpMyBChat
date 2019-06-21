2a9ee05aef2ad84a4b06943fad9c64244fa32a21
Clem-Fern <clemfern456@gmail.com>
2019-06-21 16:45:29 +0200

    Update MySQL prefixHistory table

--- src/main/java/fr/pumpmybchat/ChatManager.java
+++ src/main/java/fr/pumpmybchat/ChatManager.java
@@ -65,7 +65,7 @@ public class ChatManager implements Listener {
 	private void initMySql() throws Exception {
 		String createtable = "CREATE TABLE IF NOT EXISTS `prefixplayer` ( `uuid` VARCHAR(50) NOT NULL , `prefix` VARCHAR(50) NOT NULL DEFAULT 'I-LOVE-PMK' , `active` BOOLEAN NOT NULL DEFAULT '1' , `modification` INT NOT NULL , PRIMARY KEY (`uuid`)) ENGINE = InnoDB;";
 		this.mySQL.sendUpdate(createtable);	
-		createtable = "CREATE TABLE IF NOT EXISTS `prefixhistory` ( `uuid` VARCHAR(50) NOT NULL , `prefix` VARCHAR(50) NOT NULL , `date` DATETIME NOT NULL DEFAULT NOW() ) ENGINE = InnoDB;";
+		createtable = "CREATE TABLE IF NOT EXISTS `prefixhistory` ( `uuid` VARCHAR(50) NOT NULL , `prefix` VARCHAR(50) NOT NULL , `active` BOOLEAN NOT NULL , `date` DATETIME NOT NULL DEFAULT NOW() ) ENGINE = InnoDB;";
 		this.mySQL.sendUpdate(createtable);
 		createtable = "CREATE TABLE IF NOT EXISTS `nickhistory` ( `uuid` VARCHAR(50) NOT NULL , `nick` VARCHAR(50) NOT NULL , `date` DATETIME NOT NULL DEFAULT NOW() ) ENGINE = InnoDB;";
 		this.mySQL.sendUpdate(createtable);
