d528e75672a2ef4acdb8f7e65bc274e01f0f3af6
Clem-Fern <clemfern456@gmail.com>
2019-06-26 12:53:44 +0200

    Fix project warnings

--- src/main/java/fr/pumpmybchat/utils/InsufisantModificationPrefixException.java
+++ src/main/java/fr/pumpmybchat/utils/InsufisantModificationPrefixException.java
@@ -4,6 +4,11 @@ import fr.pumpmybchat.chat.Prefix;
 
 public class InsufisantModificationPrefixException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3280714866942216948L;
+
 	public InsufisantModificationPrefixException(Prefix prefix) {
 		// TODO Auto-generated constructor stub
 	}
