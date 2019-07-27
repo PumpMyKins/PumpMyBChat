bf212698cdab80e503f0cdec80c25c8d5cec48da
Clem-Fern <clemfern456@gmail.com>
2019-07-27 02:15:05 +0200

    Update imports/constructors in InsufisantModificationException class

--- src/main/java/fr/pumpmybchat/utils/InsufisantModificationException.java
+++ src/main/java/fr/pumpmybchat/utils/InsufisantModificationException.java
@@ -1,15 +1,20 @@
 package fr.pumpmybchat.utils;
 
+import fr.pumpmybchat.chat.Nickname;
 import fr.pumpmybchat.chat.Prefix;
 
-public class InsufisantModificationPrefixException extends Exception {
+public class InsufisantModificationException extends Exception {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -3280714866942216948L;
 
-	public InsufisantModificationPrefixException(Prefix prefix) {
+	public InsufisantModificationException(Prefix prefix) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InsufisantModificationException(Nickname nickname) {
 		// TODO Auto-generated constructor stub
 	}
 
