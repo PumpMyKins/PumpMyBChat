c9c428ccfb3aaad0c2b3855244f7cd2827797c17
Clem-Fern <clemfern456@gmail.com>
2019-07-29 01:41:04 +0200

    Add HelpNickAdminSubCommand class

---
+++ src/main/java/fr/pumpmybchat/command/nick/admin/HelpNickAdminSubCommand.java
@@ -0,0 +1,49 @@
+package fr.pumpmybchat.command.nick.admin;
+
+import java.util.List;
+
+import fr.pumpmybchat.Main;
+import fr.pumpmybchat.command.utils.ISubCommand;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.HoverEvent.Action;
+import net.md_5.bungee.api.plugin.Command;
+
+public class HelpNickAdminSubCommand implements ISubCommand{
+
+	@Override
+	public void onSubCommand(Command exec, CommandSender sender, List<String> args) {
+
+		TextComponent header = new TextComponent(Main.PLUGIN_PREFIX);
+		TextComponent message = new TextComponent("----[HELP]Nick-Admin[HELP]----");
+		message.setColor(ChatColor.GOLD);
+		message.setBold(true);
+		header.addExtra(message);
+		
+		sender.sendMessage(header);
+		
+		TextComponent block = new TextComponent("/nick-admin block");
+		block.setColor(ChatColor.DARK_BLUE);
+		block.setBold(true);
+		block.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/nick-admin block"));
+		block.setHoverEvent(new HoverEvent(Action.SHOW_TEXT,new ComponentBuilder("§bExecuter la commande !").create()));
+		TextComponent txt = new TextComponent(" pour bloquer ou débloquer le surnom d'un joueur.");
+		txt.setColor(ChatColor.AQUA);
+		block.addExtra(txt);
+		
+		sender.sendMessage(block);	
+
+	}
+
+	public void onSubCommand(Command motdCommandExecutor, CommandSender sender) {
+
+		this.onSubCommand(motdCommandExecutor, sender, null);
+
+	}
+
+
+}
